05592e46b85ef1276f298df7a335675879701458
LACRM <lacrm@LACRMs-MacBook-Pro.local>
2026-05-22 15:45:24 -0500

    Initial commit

---
+++ file2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html> 
+	
+	<head>
+		
+		<meta charset="utf-8"> 
+		<title>Annaliese Info</title>
+		
+		<style></style>
+	</head>
+
+
+	<body>
+	
+		<p>
+			hello this is the second
+
+            page 
+		</p>
+
+    
+	</body>
---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html> 
+	
+	<head>
+		
+		<meta charset="utf-8"> 
+		
+		<title>Annaliese Info</title>
+		<link rel="stylesheet" href="style.css">
+
+		<style>
+        </style>
+        
+	
+	</head>
+
+	<body>
+	
+        <div class="full-screen-block">
+            <h1>Welcome to Annaliese's Blog</h1>
+            <!-- how can i make there be less space between these two words? -->
+            <h2>Click an image to discover more about me!</h2>
+
+        <div class="main-container"> 
+            
+            <div class ="column">
+                <a href="https://www.youtube.com/watch?v=7Wv5G58YRAU&list=RD7Wv5G58YRAU&start_radio=1" target="_blank">
+                    <img src="musicIcon.jpg">
+                </a>
+
+                <a href="https://www.visitsanantonio.com/things-to-do/" target="_blank">
+                    <img src="hometownIcon.jpg">
+
+                </a>
+            </div>
+            
+            <div class="column">
+                <a href="file2.html" target="_blank">
+                    <img src="readingIcon.jpg">
+                </a>
+
+                <a href="file2.html" target="_blank">
+                    <img src="poetryIcon.jpg">
+                </a>
+
+                 <a href="file2.html" target="_blank">
+                    <img src="friendsIcon.jpg">
+                </a>
+            </div>
+            
+            <div class="column">
+                <a href="movies-page.html" target="_blank">
+                    <img src="moviesIcon.jpg">
+                </a>
+
+                <a href="file2.html" target="_blank">
+                    <img src="washuIcon.jpg">
+                </a>
+
+
+            </div>
+    
+          </div>
+
+            <a href="https://www.youtube.com/watch?v=7Wv5G58YRAU&list=RD7Wv5G58YRAU&start_radio=1" class="youtube-button" target="_blank">My YouTube Playlist</a>
+            <a href="file2.html" target="_blank">This is a link to file2</a>
+    
+        </div>
+
+	</body>
+	
+</html>
